a1deb266da41ef28ddcc04853de64a450d50b66f
corcorjp22 <161895024+corcorjp22@users.noreply.github.com>
2024-06-12 13:48:58 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('quote-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your request. We will get back to you soon!');
+    // Here you can add code to handle form submission, e.g., send data to your server
+});
